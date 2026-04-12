2f2b3612352534291c3e24281d21ace3bb084b51
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2026-04-12 17:44:06 +0530

    update homepage title and add main section layout

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,20 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResumeIQ" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover" >
+    <section className="main-section" >
+      <div className="page-heading py-16" >
+        <h1>Own Your Job Search Journey</h1>
+        <h2>Track applications, analyze performance, and get AI-powered resume ratings.</h2>
+      </div>
+    </section>
+
+  </main>
 }
